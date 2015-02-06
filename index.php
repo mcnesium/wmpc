361650fbe3f2a61f052b89af91f77827838127af
mcnesium <mcnesium@versteht.es>
2015-02-06 14:17:46 +0100

    cleanup and comment code

--- index.php
+++ index.php
@@ -1,25 +1,37 @@
+<!DOCTYPE html>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="robots" content="noindex">
+<title>WMPC</title>
 <?php
 
 if ( isset($_POST['mpc']) ) {
 
+    // escape post variable
     $post = escapeshellarg($_POST['mpc']);
 
-    echo '<pre>';var_dump($post);echo '</pre>';
+    // execute shell command to play the selected stream
     shell_exec('mpc clear && mpc load streams/di.fm/'.$post.' && mpc play');
 
 }
 
-$lsdi = explode( "\n", shell_exec('ls -1 /mnt/crypt/musik/streams/di.fm/') );
+?>
+
+<?php
+
+    // get available streams
+    $lsdi = explode( "\n", shell_exec('ls -1 /mnt/crypt/musik/streams/di.fm/') );
 
 ?>
 
 <form action="index.php" method="post">
-<select name="mpc">
-    <?php
-        foreach ($lsdi as $stream) {
-            echo '<option value="'.$stream.'">'.$stream.'</option>';
-        }
-    ?>
-</select>
-<input type="submit" value="send">
+    <select name="mpc">
+        <?php
+            // list available streams
+            foreach ($lsdi as $stream) {
+                echo '<option value="'.$stream.'">'.$stream.'</option>';
+            }
+        ?>
+    </select>
+    <input type="submit" value="send">
 </form>
