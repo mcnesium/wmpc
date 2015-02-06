480e942d259cdc99fca31785188a14f59b579adc
mcnesium <mcnesium@versteht.es>
2015-02-06 13:33:39 +0100

    di stream name as post variable

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-shell_exec('mpc stop');
+
+if ( isset($_POST['mpc']) ) {
+    echo '<pre>';var_dump($_POST['mpc']);echo '</pre>';
+    shell_exec('mpc clear && mpc load streams/di.fm/'.$_POST['mpc'].'.m3u && mpc play');
+}
 
 ?>
+
+<form action="index.php" method="post">
+<input type="text" id="mpc" name="mpc" value="">
+<input type="submit" value="send">
+</form>
